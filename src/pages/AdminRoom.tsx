0d775d20c16e16be2ab813972d1d6e360df754de
LeoMdrs <leandro.mdrs06@gmail.com>
2021-07-26 18:35:11 -0300

    Aula 04: Adicionar função de remover pergunta na tela do admin

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom'
 
 import logoImg from '../assets/images/logo.svg';
-// import deleteImg from '../assets/images/delete.svg';
+import deleteImg from '../assets/images/delete.svg';
 
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
@@ -9,7 +9,7 @@ import { Question } from '../components/Question';
 
 // import { useAuth } from '../hooks/useAuth';
 import { useRoom } from '../hooks/useRoom';
-// import { database } from '../services/firebase';
+import { database } from '../services/firebase';
 
 import '../styles/room.scss';
 
@@ -24,6 +24,12 @@ export function AdminRoom() {
   const roomId = params.id;
 
   const { title, questions } = useRoom(roomId)
+
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    }
+  }
 
   return (
     <div id="page-room">
@@ -50,7 +56,14 @@ export function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
-              />
+              >
+                <button
+                  type="button"
+                  onClick={() => handleDeleteQuestion(question.id)}
+                >
+                  <img src={deleteImg} alt="Remover pergunta" />
+                </button>
+              </Question>
             );
           })}
         </div>
