19fedb3586a2941b3aa99d0fad4b70f164376a13
LeoMdrs <leandro.mdrs06@gmail.com>
2021-07-26 17:47:59 -0300

    Aula 03: Criar página AdminRoom + Add propriedade isOutlined em componente Buttom

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,16 @@ import { ButtonHTMLAttributes } from 'react'
 
 import '../styles/button.scss';
 
-// Definindo o tipo de propriedade que o botão recebe, que nesse caso é o ButtonHTMLAttributes (= a todos os atributos de um botão html)
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+// Definindo os tipos de propriedades que o botão recebe, que nesse caso é o ButtonHTMLAttributes (= a todos os atributos de um botão html) e o isOutlined
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    isOutlined?: boolean
+};
 
-export function Button(props: ButtonProps) {
+export function Button({ isOutlined = false, ...props }: ButtonProps) {
     return (
-        <button className="button" {...props} />
+        <button
+            className={`button ${isOutlined ? 'outlined' : ''}`}
+            {...props}
+        />
     )
 }
