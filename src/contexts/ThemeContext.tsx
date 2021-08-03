2581307fbb5fbbd406019c2374dd816f7d21d112
LeoMdrs <leandro.mdrs06@gmail.com>
2021-08-03 20:38:52 -0300

    Salvando tema no localStorage

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { createContext, ReactNode, useState } from "react";
 
 type Theme = 'light' | 'dark';
@@ -14,7 +15,17 @@ type ThemeContextType = {
 export const ThemeContext = createContext({} as ThemeContextType);
 
 export function ThemeContextProvider(props: ThemeContextProviderProps) {
-    const [currentTheme, setCurrentTheme] = useState<Theme>('light');
+    const [currentTheme, setCurrentTheme] = useState<Theme>(() => {
+        // Pegando valor tema armazenado no localStorage e definindo como valor padrão
+        const storageTheme = localStorage.getItem('theme');
+
+        return (storageTheme ?? 'light') as Theme;
+    });
+
+    // Salvando tema escolhido no localStorage
+    useEffect(() => {
+        localStorage.setItem('theme', currentTheme);
+    }, [currentTheme]);
 
     function toggleTheme() {
         setCurrentTheme(currentTheme === 'light' ? 'dark' : 'light')
